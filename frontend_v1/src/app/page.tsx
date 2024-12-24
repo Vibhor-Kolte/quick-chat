9f81913175326bd7af0e7133d018c8934e1db06e
Vibhor-Kolte <vibhorkolte@gmail.com>
2024-12-24 20:55:38 +0530

    [ft_landing_page] - quick chat landing page

--- frontend_v1/src/app/page.tsx
+++ frontend_v1/src/app/page.tsx
@@ -1,7 +1,26 @@
-export default function Home() {
+import Navbar from "@/components/base/Navbar";
+import HeroSection from "@/components/base/HeroSection";
+import FeatureSection from "@/components/base/FeatureSection";
+import UserReviews from "@/components/base/UserReviews";
+import Footer from "@/components/base/Footer";
+
+export default async function LandingPage() {
+  
   return (
-    <div>
-      Hello
+    <div className="min-h-screen flex flex-col ">
+      {/* Header */}
+      <Navbar />
+      {/* Hero Section */}
+      <HeroSection />
+
+      {/* Features Section */}
+      <FeatureSection />
+
+      {/* User Reviews Section */}
+      <UserReviews />
+
+      {/* Footer */}
+      <Footer />
     </div>
   );
 }
