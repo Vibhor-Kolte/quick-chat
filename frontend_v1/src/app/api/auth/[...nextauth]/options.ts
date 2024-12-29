3598b1bb3300a2777f4c52b3287842d8258fe2eb
Vibhor-Kolte <vibhorkolte@gmail.com>
2024-12-29 20:17:19 +0530

    [ft_login_api] - working login api

--- frontend_v1/src/app/api/auth/[...nextauth]/options.ts
+++ frontend_v1/src/app/api/auth/[...nextauth]/options.ts
@@ -1,7 +1,10 @@
-import NextAuth, { AuthOptions, ISODateString } from "next-auth";
+import NextAuth, { Account, AuthOptions, ISODateString } from "next-auth";
 import { JWT } from "next-auth/jwt";
+import axios, { AxiosError } from "axios";
+import { redirect } from "next/navigation";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import { LOGIN_URL } from "@/lib/apiEndPoints";
 
 export interface CustomSession {
     user?: CustomUser;
@@ -21,14 +24,41 @@ export const authOptions: AuthOptions = {
         signIn: '/'
     },
     callbacks: {
-        async signIn({ user, account }) {
-            console.log('signIn:- ', user, account)
-            return true
+        async signIn({
+            user,
+            account,
+        }: {
+            user: CustomUser;
+            account: Account | null;
+        }) {
+            try {
+                const payload = {
+                    email: user.email!,
+                    name: user.name!,
+                    oauth_id: account?.providerAccountId!,
+                    provider: account?.provider!,
+                    image: user?.image,
+                };
+
+                const { data } = await axios.post(LOGIN_URL, payload);
+
+                user.id = data?.user?.id?.toString();
+                user.token = data?.user?.token;
+                return true;
+                
+            } catch (error) {
+                if (error instanceof AxiosError) {
+                    return redirect(`/auth/error?message=${error.message}`);
+                }
+                return redirect(
+                    `/auth/error?message=Something went wrong.please try again!`
+                );
+            }
         },
-        async session({ session, user, token }:{ 
-            session: CustomSession, 
-            user: CustomUser, 
-            token: JWT 
+        async session({ session, user, token }: {
+            session: CustomSession,
+            user: CustomUser,
+            token: JWT
         }) {
             return session
         },
@@ -39,25 +69,25 @@ export const authOptions: AuthOptions = {
             return token;
         }
     },
-  // Configure one or more authentication providers
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
-    // ...add more providers here
-    GoogleProvider({
-        clientId: process.env.GOOGLE_ID as string,
-        clientSecret: process.env.GOOGLE_SECRET as string,
-        // Force Google to re-issue a Refresh Token on each sign in
-        authorization: {
-            params: {
-              prompt: "consent",
-              access_type: "offline",
-              response_type: "code"
-            }
-          }
+    // Configure one or more authentication providers
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID!,
+            clientSecret: process.env.GITHUB_SECRET!,
         }),
-  ],
+        // ...add more providers here
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID as string,
+            clientSecret: process.env.GOOGLE_SECRET as string,
+            // Force Google to re-issue a Refresh Token on each sign in
+            authorization: {
+                params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code"
+                }
+            }
+        }),
+    ],
 }
 export default NextAuth(authOptions)
