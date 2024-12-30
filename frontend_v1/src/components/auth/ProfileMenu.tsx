6fde684d9d88c394e2977f72590a33ffe26fb918
Vibhor-Kolte <47689130+Vibhor-Kolte@users.noreply.github.com>
2024-12-30 13:31:26 +0530

    [ft_dashboard] - add dashboard nav with profile-menu (#5)
    * [ft_dashboard] - add dashboard nav with profile-menu
    
    * [ft_dashboard] - wrap logout modal inside suspense

---
+++ frontend_v1/src/components/auth/ProfileMenu.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React, { Suspense, useState } from "react";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import UserAvatar from "@/components/common/UserAvatar";
+import dynamic from "next/dynamic";
+
+const LogoutModal = dynamic(() => import("../auth/LogoutModal"));
+
+export default function ProfileMenu({
+    image,
+    name,
+}: {
+    image?: string;
+    name: string;
+}) {
+    const [logoutOpen, setLogoutOpen] = useState(false);
+    return (
+        <>
+            {logoutOpen && <Suspense fallback={<p>Loading...</p>}>
+                <LogoutModal open={logoutOpen} setOpen={setLogoutOpen} />
+            </Suspense>}
+            <DropdownMenu>
+                <DropdownMenuTrigger>
+                    <UserAvatar name={name} image={image} />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>Profile</DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setLogoutOpen(true)}>
+                        Logout
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </>
+    );
+}
