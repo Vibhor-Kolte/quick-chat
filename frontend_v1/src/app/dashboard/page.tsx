9e0b49c64b294033edabe0acbc9fd8dc4896b286
Vibhor-Kolte <vibhorkolte@gmail.com>
2025-01-01 23:57:12 +0530

    [ft_chat_group] - show group chats on dashboard

--- frontend_v1/src/app/dashboard/page.tsx
+++ frontend_v1/src/app/dashboard/page.tsx
@@ -3,9 +3,15 @@ import { authOptions, CustomSession } from '../api/auth/[...nextauth]/options';
 import { getServerSession } from 'next-auth';
 import DashNav from '@/components/dashboard/DashNav';
 import CreateChat from '@/components/chatGroup/CreateChat';
+import { fetchChatGroups } from '@/fetch/groupFetch';
+import GroupChatCard from '@/components/chatGroup/GroupChatCard';
 
 const dashboard = async () => {
     const session: CustomSession | null = await getServerSession(authOptions);
+    const groups: Array<GroupChatType> | [] = await fetchChatGroups(
+        session?.user?.token!
+    );
+
     return (
         <div>
             <DashNav
@@ -16,6 +22,14 @@ const dashboard = async () => {
                 <div className="mt-6 text-end">
                     <CreateChat user={session?.user!} />
                 </div>
+            </div>
+
+            {/* If Groups */}
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                {groups.length > 0 &&
+                    groups.map((item, index) => (
+                        <GroupChatCard group={item} key={index} user={session?.user!} />
+                    ))}
             </div>
         </div>
     )
